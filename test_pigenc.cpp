bbdd1f351b68c7a1dc5b4be7c0ab83772736bb6c
Brett Viren <brett.viren@gmail.com>
2021-08-09 17:20:18 -0400

    Round trip pigenc

--- test_pigenc.cpp
+++ test_pigenc.cpp
@@ -5,13 +5,6 @@
 
 int main(int argc, char* argv[])
 {
-    std::ifstream fstr(argv[1]);
-
-    auto dat = pigenc::read_header(fstr);
-    std::cerr << dat.dump(4) << std::endl;
-
-    auto h = pigenc::make_header("<i8", {2,2});
-    std::cerr << h << std::endl;    
 
     assert(pigenc::dtype<int>() == "<i4");
     assert(pigenc::dtype<short>() == "<i2");
@@ -20,12 +13,36 @@ int main(int argc, char* argv[])
     assert(pigenc::dtype<float>() == "<f4");
     assert(pigenc::dtype<double>() == "<f8");
 
-    std::ofstream out(argv[2]);
-    int data[4] = {0,1,2,3};
-    pigenc::write_data(out, data, {2,2});
+    {
+        std::vector<size_t> shape{2,2};
+        auto head = pigenc::make_header("<i4", shape);
+        std::cerr << head << std::endl;    
+
+        std::ofstream out("test_pigenc.npy");
+        int data[4] = {0,1,2,3};
+        pigenc::write_data(out, data, {2,2});
+    }
+
+    {
+
+        std::ifstream fstr("test_pigenc.npy");
+
+        auto head = pigenc::read_header(fstr);
+        std::cerr << head.dump(4) << std::endl;
+
+        size_t size=1;
+        for (auto s : head["shape"]) {
+            int one = s;
+            size *= one;
+        }
+        int* data = new int[size];
+    
+        fstr.read((char*)data, size*sizeof(int));
+        for (int ind=0; ind<4; ++ind) {
+            std::cerr << ind << ": " << data[ind] << std::endl;
+        }
+    }
 
     return 0;
-
-
 }
 
